8a46e6ca16572f9a1572b7de607d284b9bea9da3
S. Chang <78457278+schang-chapman@users.noreply.github.com>
2023-02-17 15:31:13 -0800

    Close files complete

--- syscpy.c
+++ syscpy.c
@@ -2,26 +2,25 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <string.h>
-#include <sys/syscall.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 
-int main(int argc, char *paths[])
-{
+int main(int argc, char *paths[]) {
     // Get paths from input.
     char *inPath = paths[1];
     char *outPath = paths[2];
 
     // Open input file to read, throw error if can't.
-    int openIn = open(inPath, O_RDONLY);
-    if (openIn == -1) {
+    int inFile = open(inPath, O_RDONLY);
+    if (inFile == -1) {
         printf("Error: %s\n", strerror(errno));
         return -1;
     }
 
     // Open output file to write, create file if doesn't exist.
     // Throw error if can't.
-    int openOut = open(outPath, O_WRONLY | O_CREAT);
-    if (openOut == -1) {
+    int outFile = open(outPath, O_WRONLY | O_CREAT);
+    if (outFile == -1) {
         printf("Error: %s\n", strerror(errno));
         return -1;
     }
@@ -29,6 +28,14 @@ int main(int argc, char *paths[])
     // Write to output file.
 
     // Close input and output files.
+    if (close(inFile) < 0) {
+        printf("Error: %s\n", strerror(errno));
+        return -1;
+    }
+    if (close(outFile) < 0) {
+        printf("Error: %s\n", strerror(errno));
+        return -1;
+    }
 
     return 0;
 }
