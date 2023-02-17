db8ffd6bf52ff238e3ccc5a87369b7c5ab8fa046
S. Chang <78457278+schang-chapman@users.noreply.github.com>
2023-02-17 15:21:17 -0800

    Open input and output file complete

--- README.md
+++ README.md
@@ -5,6 +5,12 @@
 
 This project uses system calls to copy the contents of one file into another file. It is written in C.
 
+### References
+* System call usage
+    * https://www.geeksforgeeks.org/input-output-system-calls-c-create-open-close-read-write/
+* strerror() usage
+    * https://www.tutorialspoint.com/c_standard_library/c_function_strerror.htm
+
 ### Source File(s)
 * syscpy.c
 
--- syscpy.c
+++ syscpy.c
@@ -12,8 +12,19 @@ int main(int argc, char *paths[])
     char *outPath = paths[2];
 
     // Open input file to read, throw error if can't.
+    int openIn = open(inPath, O_RDONLY);
+    if (openIn == -1) {
+        printf("Error: %s\n", strerror(errno));
+        return -1;
+    }
 
-    // Open output file to write, throw error if can't.
+    // Open output file to write, create file if doesn't exist.
+    // Throw error if can't.
+    int openOut = open(outPath, O_WRONLY | O_CREAT);
+    if (openOut == -1) {
+        printf("Error: %s\n", strerror(errno));
+        return -1;
+    }
 
     // Write to output file.
 
