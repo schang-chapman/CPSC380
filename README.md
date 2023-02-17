a5d9cdf98eb217033397a42c148d62c0e8c5ba72
S. Chang <78457278+schang-chapman@users.noreply.github.com>
2023-02-17 13:24:14 -0800

    Basic coding framework

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CPSC380_Assignment1
- 
+
+## Sharon Chang
+## Feb. 17, 2023 
---
+++ syscpy.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(char [] inFile, char [] outFile)
+{
+    // Try/except: Check if file exists, throw error if not
+
+    // Try/except: Check if output file can be written, throw error if not
+
+    // Write to output file
+}
